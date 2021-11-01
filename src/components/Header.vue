1e945394024ebbd14005cf3845c055629873b606
Mason Kim <sungjinkimm@gmail.com>
2021-11-01 13:19:38 -0400

    api fetch complete

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold mb-3">Covid-19 Tracker</div>
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fas fa-virus"></i>
+
+      Covid-19 Tracker
+    </div>
     <p>
       API by
       <a class="text-blue-300" href="https://covid19api.com" target="_blank"
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="home">Hello World from home page</div>
+  <!-- {{ dateBuilder(dataDate) }}
+  new deaths: {{ stats.newDeaths }} -->
+  <main v-if="!loading">show Data</main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      fetching Data
+      <img :src="loadingImage" class="w-24 m-auto" alt="" />
+    </div>
+  </main>
 </template>
 
 <script>
@@ -9,5 +17,35 @@
 export default {
   name: 'Home',
   components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif'),
+    };
+  },
+  methods: {
+    async fetchData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      // console.log(data);
+      return data;
+    },
+    dateBuilder(date) {
+      let newDate = new Date(date).toDateString();
+      return newDate;
+    },
+  },
+  async created() {
+    const data = await this.fetchData();
+    console.log(data);
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
 };
 </script>
